6f5e2f5b50a5cf2be943fad92eab0d5f1d26a941
Jagadeesh kj <70704500+JagadeeshKJ@users.noreply.github.com>
2022-11-10 21:21:19 +0530

    Update Requests.js

--- src/components/Requests.js
+++ src/components/Requests.js
@@ -1,4 +1,4 @@
-// const API_KEY = "your-API-key-here";
+// const API_KEY = "your-TMDB-API-key-here";
 
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
